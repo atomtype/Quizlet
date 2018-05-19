22806773f00ac5888f03089ec4f4c16303235ab3
atomtype <atomtype@github.com>
2018-05-19 12:27:14 +0530

    first commit

---
+++ quiz/other_questions.js
@@ -0,0 +1,30 @@
+
+
+
+
+addproblem("What is the correct HTML for referring to an external style sheet?",'c',"<stylesheet>mystyle.css</stylesheet>","<style src="mystyle.css">","<link rel="stylesheet" type="text/css" href="mystyle.css">")
+
+
+
+
+addproblem("What is the correct CSS syntax for making all the <p> elements bold?",'b',"<p style="font-size:bold;">","p {font-weight:bold;}","p {text-size:bold;}")
+
+
+addproblem("What is the correct HTML for adding a background color?",'c',"<background>yellow</background>","<body style="background-color:yellow;">","<body bg="yellow">")
+addproblem("What is the correct HTML for creating a hyperlink?",'a',"<a href="www.quizlet.com">W3Schools</a>","<a name="www.quizlet.com"></a>","<a url="www.quizlet.com"></a>")
+
+
+addproblem("How do you write "Hello World" in an alert box?",'c',"msgBox("Hello World");","alertBox("Hello World");","alert("Hello World");")
+addproblem("How do you call a function named "myFunction"?",'b',"call function myFunction()","myFunction()","call myFunction()")
+
+
+addproblem("How do you select an element with id "demo"?",'a',".demo","*demo","#demo")
+addproblem("How does a WHILE loop start?",'c',"while i = 1 to 10","while (i <= 10; i++)","while (i <= 10)")
+addproblem("How can you add a comment in a JavaScript?",'a',"//This is a comment","<!--This is a comment-->","'This is a comment")
+addproblem("What is the correct way to write a JavaScript array?",'a',"var colors = "red", "green", "blue"","var colors = 1 = ("red"), 2 = ("green"), 3 = ("blue")","var colors = ["red", "green", "blue"]")
+addproblem("JavaScript is the same as Java.",'b',"True","False")
+addproblem("Which operator is used to assign a value to a variable?",'c',"*","x","=")
+addproblem("What is the correct HTML for making a checkbox?",'c',"<input type="check">","<checkbox>","<input type="checkbox">")
+addproblem("What is the correct HTML for inserting an image?",'a',"<img src="image.gif" alt="MyImage">","<img href="image.gif" alt="MyImage">","<image src="image.gif" alt="MyImage">")
+addproblem("An <iframe> is used to display a web page within a web page.",'b',"True","False","There is no such thing as an <iframe>")
+addproblem("HTML comments start with <!-- and end with -->",'a',"True","False")
